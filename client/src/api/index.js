0709a5c47a4d8631982873f6017718f7316c03b2
SlumpG <shlomo.gangul1@gmail.com>
2021-08-12 14:07:33 +0300

    idk whats happening

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://moment-pro.herokuapp.com/api/posts/";
+const url = "https://moment-pro.herokuapp.com/api/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
