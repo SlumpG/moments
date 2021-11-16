facd83fe1dd5ce0f386abd55005ad07d0267d6a7
SlumpG <shlomo.gangul1@gmail.com>
2021-11-17 00:09:00 +0200

    done for real

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -61,7 +61,7 @@ const PostDetalis = () => {
                 <Typography gutterBottom variant='subtitle2'>{name}</Typography>
                 <Typography gutterBottom variant='subtitle2'>{message}</Typography>
                 <Typography gutterBottom variant='subtitle1'>Likes:{likes.length}</Typography>
-                <img src={selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} width="200px" alt="image" />
+                <img src={selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} width="200px" alt={title} />
                 </div>
             ))}
           </div>
