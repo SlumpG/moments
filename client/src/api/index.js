93f3729aca38d660f1bdf013142523960bfbecd2
SlumpG <shlomo.gangul1@gmail.com>
2021-11-18 17:28:53 +0200

    deplying

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://moment-pro.herokuapp.com/api",
 });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
