29e81233534f7042ea6f606bda67fe9f88c2c009
SlumpG <shlomo.gangul1@gmail.com>
2021-11-18 17:10:26 +0200

    small fix with routing

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,11 @@ function App() {
     <Container maxWidth="xl">
       <Navbar/>
       <Switch>
+        <Route path='/auth' exact component={()=>(!user ? <Auth/> : <Redirect to="/posts" />)} />
         <Route path='/' exact component={()=> <Redirect to="/posts" />} />
         <Route path='/posts' exact component={Home} />
         <Route path='/posts/search' exact component={Home} />
-        <Route path='/posts/:id'  component={PostDetails} />
-        <Route path='/auth' exact component={()=>(!user? <Auth/>: <Redirect to="/posts" />)} />
+        <Route path='/posts/:id' exact component={PostDetails} />
       </Switch>
     </Container>
     </Router>
